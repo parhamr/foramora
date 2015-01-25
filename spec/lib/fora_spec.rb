96a02a30419a3923f42ca1c53dc1f1d30a9f7581
parhamr <reid@copiousinc.com>
2015-01-25 11:00:22 -0800

    Using test foræ definition

--- spec/lib/fora_spec.rb
+++ spec/lib/fora_spec.rb
@@ -9,25 +9,18 @@ describe Fora do
     its(:test_uris) { should be_an Array }
   end
 
-  let(:foræ) { Fora.foræ }
-  let(:valid_options) { foræ.detect { |f| f[:fqdn] =~ /example\.org/ } }
+  let(:valid_options) { YAML.load(ERB.new(File.binread('spec/fixtures/foræ.yaml')).result)[:foræ].first }
 
   describe '[instance]' do
     after do
       subject.try(:teardown)
     end
 
-    context 'with selenium disabled' do
-      before do
-        expect(Selenium::WebDriver).to receive(:for).and_return(true)
-      end
+    context 'with valid options' do
+      subject { Fora.new(valid_options) }
 
-      context 'with valid options' do
-        subject { Fora.new(valid_options) }
-
-        it 'successfully initializes' do
-          expect { subject }.to_not raise_error
-        end
+      it 'successfully initializes' do
+        expect { subject }.to_not raise_error
       end
     end
   end
